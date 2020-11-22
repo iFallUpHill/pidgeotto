09337100edf01a7eada03eb2f23b1307222d449d
Jason Manson-Hing <jason.mansonhing@gmail.com>
2020-11-22 00:07:28 -0500

    updated dotenv to work with pm2

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
-require('dotenv').config()
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const fetch = require('node-fetch');
+const CronJob = require('cron').CronJob;
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// let timeLastRan = Date.now();
-let timeLastRan = Date.now()- 1000000000;
+let timeLastRan = Date.now();
 let resultsSent = 0;
+
+const listMonitorDeals = async () => {
+  const response = await fetch('https://www.reddit.com/r/buildapcsales/new.json?limit=50');
+  const json = await response.json();
+  const lastRanAsSeconds = Math.floor(timeLastRan / 1000);
+  const channel = client.channels.cache.find(channel => channel.name === 'bapcsales-monitors');
+
+  for (const { data } of json.data.children) {
+    if (data.created_utc > lastRanAsSeconds && data.title.toLowerCase().startsWith("[monitor]")) {
+      resultsSent++;
+      const embed = new Discord.MessageEmbed()
+        .setAuthor(data.domain, '', data.url)
+        .setTitle('Reddit Link')
+        .setURL(`https://reddit.com${data.permalink}`)
+        .setDescription(data.title.replace(/\[\bmonitor\b\] ?/mi,''))
+        .setThumbnail(['self', 'default', 'nsfw'].indexOf(data.thumbnail) !== -1 ? 'https://www.redditstatic.com/new-icon.png' : data.thumbnail)
+      channel.send(embed)
+    }
+  }
+
+  timeLastRan = Date.now();
+  console.log(`[${new Date(timeLastRan).toUTCString()}] Send ${resultsSent} listing${resultsSent === 1 ? '' : 's'}.`);
+  resultsSent = 0;
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
+  listMonitorDeals();
 
-client.on('message', async (msg) => {
-  if (msg.content === 'new') {
-    const response = await fetch('https://www.reddit.com/r/buildapcsales/new.json?limit=50');
-    const json = await response.json();
+  const monitorDealsJob = new CronJob('0 */1 * * * *', () => {
+    listMonitorDeals();
+  });
 
-    for (const { data } of json.data.children) {
-      if (data.created_utc * 1000 > timeLastRan && data.title.toLowerCase().startsWith("[monitor]")) {
-        resultsSent++;
-        const embed = new Discord.MessageEmbed()
-          .setAuthor(data.domain, '', data.url)
-          .setTitle('Reddit Link')
-          .setURL(`https://reddit.com${data.permalink}`)
-          .setDescription(data.title.replace(/\[\bmonitor\b\] ?/mi,''))
-          .setThumbnail(['self', 'default', 'nsfw'].indexOf(data.thumbnail) !== -1 ? 'https://www.redditstatic.com/new-icon.png' : data.thumbnail)
-        msg.reply(embed);
-      }
-    }
-
-    timeLastRan = Date.now();
-    console.log(`[${new Date(timeLastRan).toUTCString()}] Send ${resultsSent} listing${resultsSent === 1 ? '' : 's'}.`);
-    resultsSent = 0;
-  }
+  monitorDealsJob.start();
 });
 
 client.on('warn', console.warn);
